28e79d2f926d224c74834f5a3b0a4cd9b0dd11c6
Dennis Fashimpaur <dfashimpaur@gmail.com>
2025-11-12 08:55:11 -0600

    First pass for Anthropic Chat Agent

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+load_dotenv()
+
+llm = ChatAnthropic(model_name="claude-sonnet-4-5-20250929",
+                    temperature=0,
+                    timeout=None,
+                    max_retries=2)
+response = llm.invoke("Tell me a joke")
+print(response)
